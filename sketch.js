d3cd4005df124fc4cbf80af2218ea3f04cfaf6be
Shaneel Kumar <shaneel_kumar@live.com>
2022-03-08 23:55:41 +1300

    Added reset button functionality, as well as different color modes and associated functionality.

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 // Buttton hover effect. Need to omit if button is on dark mode.
 const buttons = document.querySelectorAll('.options');
 buttons.forEach(element => element.addEventListener('mouseover', () => {
@@ -17,25 +21,88 @@ buttons.forEach(element => element.addEventListener('mouseup', () => {
     element.classList.remove('button-click');
 }));
 
+// Total width and height for grid in pixels.
+let totalGridDimensions = 350;
 
-let gridDimensions = '350px';
-
+// Initial creation of grid.
 const pixelGrid = document.createElement('div');
 pixelGrid.classList.add('pixel-grid');
-pixelGrid.style.width = gridDimensions;
-pixelGrid.style.height = gridDimensions;
+pixelGrid.style.width = totalGridDimensions;
+pixelGrid.style.height = totalGridDimensions;
 
-const gridDivisions = 20;
-let pixelWidth = parseInt(gridDimensions) / gridDivisions;
-let pixelHeight = parseInt(gridDimensions) / gridDivisions;
+// Get & return number of divisions from the input field based on total grid width & height.
+function getGridDivs() {
+    const divValue = parseInt(document.querySelector('.divisions').value);
+    if (isNaN(divValue)) {
+        alert('Division amount is not a valid number.')
+        return
+    }
+    return divValue
+}
 
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+// Calculate individual pixel width and heights.
+const gridDivisions = getGridDivs();
+let pixelWidth = totalGridDimensions / gridDivisions;
+let pixelHeight = totalGridDimensions / gridDivisions;
 
-function changeColor(e) {
+// Color options for pixels.
+function changePixelToBlack(e) {
     if (e.type === 'mouseover' && !mouseDown) return
-    e.target.style.backgroundColor = 'black';
+    e.target.style.backgroundColor = 'black'; }
+
+function changePixelToRainbow(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    const randColorNumber = Math.floor(Math.random() * 16777215).toString(16);
+    const randColor = `#${randColorNumber}`;
+    e.target.style.backgroundColor = randColor;}
+
+function changePixelToGreyShades(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    const currentGrey = window.getComputedStyle( e.target ,null).getPropertyValue('background-color');
+    const firstSplit = currentGrey.split('(')[1];
+    const colourValue = firstSplit.split(',')[0];
+    let colourNumber = parseInt(colourValue);
+    if (colourNumber < 50) return
+    colourNumber -= 25;
+    newColorCode = `rgb(${colourNumber}, ${colourNumber}, ${colourNumber})`
+    e.target.style.backgroundColor = newColorCode;
+}
+
+function setActiveColorMode() {
+    const activeColorMode = getActiveColorButton();
+    const changePixels = pixelGrid.querySelectorAll('.pixel');
+
+    if (activeColorMode === 'Black') {
+        changePixels.forEach( function(pixel) {
+            pixel.removeEventListener('mouseover', changePixelToRainbow);
+            pixel.removeEventListener('mousedown', changePixelToRainbow);
+            pixel.removeEventListener('mouseover', changePixelToGreyShades);
+            pixel.removeEventListener('mousedown', changePixelToGreyShades);
+
+            pixel.addEventListener('mouseover', changePixelToBlack);
+            pixel.addEventListener('mousedown', changePixelToBlack);
+        });
+    } else if (activeColorMode === 'Rainbow') {
+        changePixels.forEach( function(pixel) {
+            pixel.removeEventListener('mouseover', changePixelToBlack);
+            pixel.removeEventListener('mousedown', changePixelToBlack);
+            pixel.removeEventListener('mouseover', changePixelToGreyShades);
+            pixel.removeEventListener('mousedown', changePixelToGreyShades);
+
+            pixel.addEventListener('mouseover', changePixelToRainbow);
+            pixel.addEventListener('mousedown', changePixelToRainbow);
+        });
+    } else if (activeColorMode === 'Grey') {
+        changePixels.forEach( function(pixel) {
+            pixel.removeEventListener('mouseover', changePixelToBlack);
+            pixel.removeEventListener('mousedown', changePixelToBlack);
+            pixel.removeEventListener('mouseover', changePixelToRainbow);
+            pixel.removeEventListener('mousedown', changePixelToRainbow);
+
+            pixel.addEventListener('mouseover', changePixelToGreyShades);
+            pixel.addEventListener('mousedown', changePixelToGreyShades);
+        });
+    }
 }
 
 // Create individual pixels that populates the pixel grid.
@@ -47,7 +114,9 @@ for (let row = 0; row < gridDivisions; row ++) {
         pixel.classList.add('pixel');
         pixel.style.width = `${pixelWidth}px`;
         pixel.style.height = `${pixelHeight}px`;
-        pixel.addEventListener('mouseover', changeColor);
+        pixel.style.backgroundColor = 'rgb(255, 255, 255)';
+        pixel.addEventListener('mouseover', setActiveColorMode);
+        pixel.addEventListener('mousedown', setActiveColorMode);
         pixelRow.appendChild(pixel);
     }
     pixelGrid.appendChild(pixelRow);
@@ -55,3 +124,21 @@ for (let row = 0; row < gridDivisions; row ++) {
 
 const gridContainer = document.querySelector('.grid-container');
 gridContainer.appendChild(pixelGrid);
+
+// Reset canvas pixels.
+resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', () => {
+    const pixelsToReset = pixelGrid.querySelectorAll('.pixel');
+    pixelsToReset.forEach(pixel => pixel.style.backgroundColor = 'rgb(255, 255, 255');
+});
+
+function getActiveColorButton() {
+    buttons.forEach(function(element) {
+        if (element.classList.contains('button-click')) {
+            colorMode = element.textContent;
+        }
+    });
+    return colorMode
+}
+
+setActiveColorMode();
